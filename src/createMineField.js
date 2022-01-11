ccb4c5eca99aa27263014e581424ab7d1342ec8e
seongyeon <ysy2565@naver.com>
2022-01-12 00:45:31 +0900

    add function

--- src/createMineField.js
+++ src/createMineField.js
@@ -1,13 +1,41 @@
+const _ = require("lodash");
 function createMineField(fieldInfo) {
   // 원하는 폭탄의 개수가 게임판의 개수보다 많을 경우에 대한 처리
+  if (fieldInfo.count > fieldInfo.width * fieldInfo.height) {
+    throw new Error("입력 값이 잘못되었습니다.");
+  }
 
   // 2차원 배열을 사용자에게 입력받은 숫자에 맞게 생성한다
+  var arr = new Array(fieldInfo.height);
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = new Array(fieldInfo.width);
+    for (var j = 0; j < arr[i].length; j++) {
+      arr[i][j] = 0;
+    }
+  }
 
   // 폭탄을 2차원 배열에 랜덤하게 넣는다
+  for (var i = 0; i < fieldInfo.count; i++) {
+    var bomb = (arr[_.random(fieldInfo.height - 1)][
+      _.random(fieldInfo.width - 1)
+    ] = "*");
+  }
 
   // 폭탄이 아닌 자리들에 알맞는 숫자를 채워넣는다
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === "*") {
+        for (var a = i - 1; a <= i + 1; a++) {
+          for (var b = j - 1; b <= j + 1; b++) {
+            if (arr[a][b] != "*") arr[a][b]++;
+          }
+        }
+      }
+    }
+  }
 
   // 2차원 배열을 반환한다
+  console.log(fieldInfo.count);
 }
 
 module.exports = createMineField;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // 이 파일은 수정하시면 안됩니다.
 // 이 파일의 역할은 사용자로부터 입력을 받고 그 입력값을 createMineField 함수에 넘기는 역할입니다.
 
-const { createInterface } = require('readline');
-const createMineField = require('./createMineField');
+const { createInterface } = require("readline");
+const createMineField = require("./createMineField");
 
 const rl = createInterface({
   input: process.stdin,
@@ -16,9 +16,9 @@ const fieldInfo = {
 };
 
 // 아래 블럭은 사용자로부터 지뢰밭에 대한 정보를 입력받는 코드입니다.
-rl.question("지뢰밭의 가로 길이를 입력해주세요. ", w => {
-  rl.question("지뢰밭의 세로 길이를 입력해주세요. ", h => {
-    rl.question("지뢰의 개수를 입력해주세요. ", c => {
+rl.question("지뢰밭의 가로 길이를 입력해주세요. ", (w) => {
+  rl.question("지뢰밭의 세로 길이를 입력해주세요. ", (h) => {
+    rl.question("지뢰의 개수를 입력해주세요. ", (c) => {
       fieldInfo.width = +w;
       fieldInfo.height = +h;
       fieldInfo.count = +c;
