be39bb51235594182afd1264796882eeaf25e389
seongyeon <ysy2565@naver.com>
2022-01-12 22:18:22 +0900

    modify return

--- src/createMineField.js
+++ src/createMineField.js
@@ -15,11 +15,11 @@ function createMineField(fieldInfo) {
   // 폭탄을 2차원 배열에 랜덤하게 넣는다
   const bombarr = createBomb(fieldInfo, arr);
 
-  // 폭탄이 아닌 자리들에 알맞는 숫자를 채워넣는다
+  // 폭탄이 아닌 자리들에 알맞는 숫자를 채워넣는다s
   const numarr = createNum(bombarr);
 
   // 2차원 배열을 반환한다
-  console.log(numarr);
+  return numarr;
 }
 
 module.exports = createMineField;
